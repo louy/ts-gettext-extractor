e21a4205cc570396ed606d1fdf6ed8d2ae52ac8e
Louay Akkad <349850+louy@users.noreply.github.com>
2024-05-17 14:43:40 +0100

    Add a test for multiple files declaring the same strings

---
+++ tests/src/test.copy.ts
@@ -0,0 +1,15 @@
+const __d = (...args: any[]) => {};
+
+export default /* 
+Another comment to be extracted. However, this time it's a very long comment that can span multiple lines and might be broken into multiple lines in the output `.po` file.
+*/ __d("typescript", "Hello there!");
+
+__(`Test backticks`);
+__d("test", `Test backticks with __d`);
+
+__("'Single quote'");
+__(
+  '"Double quotes at exactly 76th character of this string... do you like that"'
+);
+__`And a
+new line!`;
